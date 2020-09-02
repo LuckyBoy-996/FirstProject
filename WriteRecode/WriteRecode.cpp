cf6195798e2ee1bfe2e5370bc2d75303d41e8b77
NiuYaqiang <niuyaqiang@aixuexi.com>
2020-09-02 14:24:13 +0800

    master分支，第四次修改

--- WriteRecode/WriteRecode.cpp
+++ WriteRecode/WriteRecode.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	//这是master分支，第3次修改
+	//这是master分支，第4次修改
 	cout<<"写日志例子程序"<<endl;
 	OutputDebugMessage(L"写日志列子 aaaaaaaaaaaa");
 	ErrorMessage("写日志列子    aaaaaaaaaaaa");
