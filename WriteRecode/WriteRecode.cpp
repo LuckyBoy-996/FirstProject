abe0f097acde86b80dac7f00bf2abd66701617f2
NiuYaqiang <niuyaqiang@aixuexi.com>
2020-09-02 15:58:04 +0800

    ddddddddddddddd

--- WriteRecode/WriteRecode.cpp
+++ WriteRecode/WriteRecode.cpp
@@ -5,15 +5,7 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	//test2分支，第一次修改
-<<<<<<< HEAD
-<<<<<<< HEAD
-	//bbbbbbbbbbbbbbbbbbbb
-	//ccccccccccccccccccc
-=======
->>>>>>> parent of 2b96ad0... bbbbbbbbbbbbbb
-=======
->>>>>>> parent of 2b96ad0... bbbbbbbbbbbbbb
+	//dddddddddddddddd
 	cout<<"写日志例子程序"<<endl;
 	OutputDebugMessage(L"写日志列子 aaaaaaaaaaaa");
 	ErrorMessage("写日志列子    aaaaaaaaaaaa");
